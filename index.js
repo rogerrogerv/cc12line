c2831d70a37cd2ba5335d01b91e1d0f0f042cab1
Saggitarie <saggitarie@gmail.com>
2020-05-20 15:19:44 +0900

    Changed path for static files

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const cors = require('cors');
 const myLiffId = process.env.MY_LIFF_ID;
 const { LineClient } = require("messaging-api-line");
 
-app.use(express.static('./frontend/build'));
+app.use(express.static('build'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join("/", "app","build", "index.html"));
+});
 
 const client = LineClient.connect({
   accessToken: process.env.ACCESS_TOKEN,
