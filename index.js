711022bb73c42da6eb8ca6ada25030d12b485d86
Yurika Namba <yurikanamba@gmail.com>
2020-05-20 18:25:54 +0900

    adding frontend

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 9000;
-const cors = require('cors');
+const cors = require("cors");
 const myLiffId = process.env.MY_LIFF_ID;
 const { LineClient } = require("messaging-api-line");
+const axios = require("axios");
 
 // const path = require('path');
 
 console.log("WHERE IS A PORT???", port);
 
-app.use(express.static('build'));
+app.use(express.static("build"));
 
 // /app/frontend/build/index.html
 // app.get('*', (req, res) => {
@@ -26,23 +27,107 @@ const client = LineClient.connect({
   channelSecret: process.env.CHANNEL_SECRET,
 });
 
-app.get('/send-id', function(req, res) {
-    console.log("IS THIS COMING HERE???");
-    res.json({id: myLiffId});
+app.get("/get-covid-status", cors(), async function (req, res) {
+  let covidReport;
+  await axios
+    .get("https://covid-193.p.rapidapi.com/statistics?country=Japan", {
+      headers: {
+        "x-rapidapi-host": "covid-193.p.rapidapi.com",
+        "x-rapidapi-key": "1c306d3386mshb1967e7c5f086f6p18a930jsncd5654c429dc",
+      },
+    })
+    .then((response) => {
+      let activeCases = response.data.response[0].cases.active || 0;
+      let newCases = response.data.response[0].cases.new || 0;
+      let newDeaths = response.data.response[0].deaths.new || 0;
+      covidReport = `DAILY COVID REPORT: There are ${activeCases} active cases, ${newCases} new cases, and ${newDeaths} new deaths.`;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  client.multicast(
+    [
+      process.env.USER_ID_Y,
+      process.env.USER_ID_S,
+      process.env.USER_ID_K,
+      process.env.USER_ID_A,
+      process.env.USER_ID_R,
+    ],
+    [
+      {
+        type: "text",
+        text: covidReport,
+      },
+    ]
+  );
 });
 
-app.get('/send-messages',cors(), function(req, res) {
-    console.log("IS IT SENDING MESSAGES???");
-    client.multicast(
-      [process.env.USER_ID_Y, process.env.USER_ID_S, process.env.USER_ID_K, process.env.USER_ID_A, process.env.USER_ID_R],
-      [
-        {
-          type: 'text',
-          text: 'You have final warning....',
-        },
-      ]
-    );
-    res.json("success");
+app.get("/send-id", function (req, res) {
+  console.log("IS THIS COMING HERE???");
+  res.json({ id: myLiffId });
+});
+
+app.get("/send-joke", cors(), async function (req, res) {
+  let joke = "";
+
+  await axios
+    .get("https://dad-jokes.p.rapidapi.com/random/jokes", {
+      // method: "GET",
+      headers: {
+        "x-rapidapi-host": "dad-jokes.p.rapidapi.com",
+        "x-rapidapi-key": "1c306d3386mshb1967e7c5f086f6p18a930jsncd5654c429dc",
+      },
+    })
+    .then((res) => {
+      return res.data;
+    })
+    .then((response) => {
+      console.log("dad joke ***********-->", response);
+      const setup = response.setup;
+      const punchline = response.punchline;
+      joke = `${setup}...${punchline}`;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  client.multicast(
+    [
+      process.env.USER_ID_Y,
+      process.env.USER_ID_S,
+      process.env.USER_ID_K,
+      process.env.USER_ID_A,
+      process.env.USER_ID_R,
+    ],
+    [
+      {
+        type: "text",
+        text: joke,
+      },
+    ]
+  );
+  res.json("success");
+});
+
+app.get("/send-messages", cors(), function (req, res) {
+  console.log("IS IT SENDING MESSAGES???");
+  client.multicast(
+    [
+      process.env.USER_ID_Y,
+      process.env.USER_ID_S,
+      process.env.USER_ID_K,
+      process.env.USER_ID_A,
+      process.env.USER_ID_R,
+    ],
+    [
+      {
+        type: "text",
+        text: "You have final warning....",
+      },
+    ]
+  );
+  res.json("success");
 });
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
